7dfa9ae618f0042fc4fb61bce9e3c2f8d414a4a0
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-10 06:44:48 +0500

    Created navabr with theme-state

---
+++ learnwithimran/src/components/Navbar/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.css'
+
+const Navbar = ({ theme }) => {
+  return (
+    <nav
+      className="navbar"
+      style={{
+        backgroundColor: theme.theme === "light" ? "white" : "black",
+        color: theme.theme === "light" ? "black" : "white",
+      }}
+    >
+      <div className="navbar-links">
+        <Link
+          to="/"
+          style={{ color: theme.theme === "light" ? "black" : "white" }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/signup"
+          style={{ color: theme.theme === "light" ? "black" : "white" }}
+        >
+          Signup
+        </Link>
+      </div>
+      <div className="navbar-toggle-theme">
+        <button
+          onClick={() =>
+            theme.setTheme(theme.theme === "light" ? "dark" : "light")
+          }
+        >
+          Toggle Theme
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
