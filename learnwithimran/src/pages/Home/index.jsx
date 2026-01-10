4c7fba38615c15759ca8a9bcecc4951e9d89b8d4
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-10 06:42:17 +0500

    Worked with useCallBack,useRef,useContext and react-router-dom with adding navbar

--- learnwithimran/src/pages/Home/index.jsx
+++ learnwithimran/src/pages/Home/index.jsx
@@ -1,17 +1,28 @@
-import { useCallback, useState } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
+import {ThemeContext} from '../../context'
+import Navbar from "../../components/Navbar";
 
 const HomePage = (props) => {
   console.log(props);
   const [count, setCount] = useState(0);
+  const inputRef = useRef(null);
+  const countRef = useRef(0);
+  const theme = useContext(ThemeContext);
 
   //* putting condition on increment
-  const increment = useCallback(() => {
-    if (count < 10) {
-      setCount(count + 1)
-    } else {
-      alert("Count is greater than 10: Limit is reached.")
-    }
-  }, [count]);
+  // const increment = useCallback(() => {
+  //   if (count < 10) {
+  //     setCount(count + 1)
+  //   } else {
+  //     alert("Count is greater than 10: Limit is reached.")
+  //   }
+  // }, [count]);
+
+  //! useRef
+  const increment = () => {
+    countRef.current += 1;
+    console.log(countRef.current);
+  }
   
   //* putting condition on decrement
   const decrement = useCallback(() => {
@@ -20,10 +31,18 @@ const HomePage = (props) => {
     } else {
       alert("Count is equal to '0': Limit is reached.Number can't be negative")
     }
-    },[count]); 
+  }, [count]);
+  
+  //! useRef
+  const handleOnFocus = () => {
+    console.log('Focus');
+    inputRef.current.focus();
+  }
   
   return (
     <div>
+      <Navbar theme={theme} />
+      {/* <div>Theme:{theme.theme}</div> */}
       <h1>I am a Home page</h1>
       <h2>Name: {props.name}</h2>
       <h2>Counter:{count}</h2>
@@ -32,7 +51,11 @@ const HomePage = (props) => {
       {/* <button onClick={() => setCount((prevValue) => prevValue + 1)}>Increment</button> */}
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
-      <button onClick={()=>setCount(0)}>Reset</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      
+      {/* input tied with ref */}
+      <input type="text" ref={inputRef} />
+      <button onClick={()=>handleOnFocus()}>Focus </button>
     </div>
   );
 };
